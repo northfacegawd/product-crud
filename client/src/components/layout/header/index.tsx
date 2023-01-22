e27b182fe38d64bf4e1613c66ad7e809a6d309ec
northfacegawd <chuck5732590@gmail.com>
2023-01-22 14:46:32 +0900

    feat: (client) 헤더 링크 추가

--- client/src/components/layout/header/index.tsx
+++ client/src/components/layout/header/index.tsx
@@ -18,8 +18,14 @@ export default function Header() {
         <LogoLink to="/">
           <Logo />
         </LogoLink>
-        <ActiveLink to="/products" active={pathname.startsWith('/products')}>
-          쇼핑하러가기
+        <ActiveLink to="/products" active={pathname === '/products'}>
+          상품 목록
+        </ActiveLink>
+        <ActiveLink
+          to="/products/upload"
+          active={pathname.startsWith('/products/upload')}
+        >
+          상품 업로드
         </ActiveLink>
         <LoginLink to="/login" active={pathname.startsWith('/login')}>
           로그인
